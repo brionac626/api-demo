b3f8ced3bf489e76bd18e2a0bc2c43a800cc7057
AutumnAoi <theone1632@gmail.com>
2024-04-17 19:14:57 +0800

    update: article repo domain

--- domains/articles.go
+++ domains/articles.go
@@ -1,10 +1,15 @@
 package domains
 
-import "context"
+import (
+	"context"
 
+	"github.com/brionac626/api-demo/models"
+)
+
+// ArticlesRepo a repository for articles operations
 type ArticlesRepo interface {
-	FindAllArticles(ctx context.Context)
-	InerstNewArticle(ctx context.Context)
-	UpdateArticle(ctx context.Context)
-	DeleteArticle(ctx context.Context, id string)
+	FindAllArticles(ctx context.Context, page, limit int) ([]models.Article, int, error)
+	InsertNewArticle(ctx context.Context, article models.Article) error
+	UpdateArticle(ctx context.Context, article models.Article) error
+	DeleteArticle(ctx context.Context, id string) error
 }
